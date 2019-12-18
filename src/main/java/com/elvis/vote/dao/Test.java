399283f34ab26046091369010c343f4371b95976
ljj7r <871127471@qq.com>
2019-12-18 15:48:21 +0800

    第一次测试提交

--- src/main/java/com/elvis/vote/dao/Test.java
+++ src/main/java/com/elvis/vote/dao/Test.java
@@ -5,8 +5,6 @@ import com.elvis.vote.pojo.User;
 import org.apache.ibatis.annotations.Select;
 
 public interface Test {
-    @Select("select * from user")
+    @Select("select * from user ")
     User test();
-
-
 }
