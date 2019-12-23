58cbb4fe62738523fd62cb4142b8ac2f46d5fb1c
e <2427510138@qq.com>
2019-12-23 10:58:55 +0800

    邮箱验证api

---
+++ src/main/java/com/elvis/vote/pojo/EmailEntity.java
@@ -0,0 +1,44 @@
+package com.elvis.vote.pojo;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class EmailEntity implements Serializable {
+
+    private Timestamp time;
+    private String acount;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "EmailEntity{" +
+                "time=" + time +
+                ", acount='" + acount + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public String getAcount() {
+        return acount;
+    }
+
+    public void setAcount(String acount) {
+        this.acount = acount;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
